38e067f5230b83fd89b98215e8a700f45a802717
manoj <manojt2409@gmail.com>
2023-11-13 17:15:08 +0530

    selection sort

--- src/sortingAlgorithms/SortingAlgorithms.java
+++ src/sortingAlgorithms/SortingAlgorithms.java
@@ -12,7 +12,8 @@ public class SortingAlgorithms {
         printArray(a);
         //bubblesort(a);
         //selectionSort(a);
-        insertionSort(a);
+        //insertionSort(a);
+
     }
     static void printArray(int[] array){
         for(int i=0;i<array.length;i++){
@@ -20,6 +21,14 @@ public class SortingAlgorithms {
         }
         System.out.println();
     }
+    static int[] swap(int arr[], int a , int b){
+        int temp=0;
+        temp=arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+        return arr;
+    }
+
     static void bubblesort(int arr[]){
        // System.out.print("bubbleSort: ");
         int length= arr.length;
@@ -36,8 +45,8 @@ public class SortingAlgorithms {
             }
         }
         System.out.println("Swap count : "+swapCounter);
-
     }
+
     static void selectionSort(int arr[]){
         //smallest to the beginning
        // System.out.print("selectionSort: ");
@@ -67,23 +76,28 @@ public class SortingAlgorithms {
     }
 
     static void insertionSort(int arr[]){
-        int length=arr.length;
-        for (int i = 0; i <length-1; i++) {
+        int n=arr.length;
+        int swap=0,temp=0;
+        for (int i = 0; i <n-1; i++) {
             for(int j=i+1;j>0;j--){
                 if(arr[j]<arr[j-1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp;
+                    swap(arr,j,j-1);
+                    swap++;
                 }
                 else break;
             }
+
         }
         printArray(arr);
+        System.out.println("Swap : "+swap);
     }
-
 }
+
+
+
 /*
 The difference between bubble sort and selection sort is that
-Bubble sort : Swapping occurs whenever it finds a number that is satisfying the above condition
-Selection sort : it swaps only the required number(that too its equal or less that the length of the array)
+Bubble sort : Swapping occurs whenever it finds a number that is satisfying the above condition(To put simply its just brute force)
+Selection sort : it swaps only the required number(that too its equal or less that the length of the array[N])
+Insertion sort : mostly used in partially sorted arrays
  */

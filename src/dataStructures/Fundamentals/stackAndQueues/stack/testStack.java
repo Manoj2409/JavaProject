3583ebce5d74fbd66438fe1eb80451e91654fbec
Manoj Thiyagarajan <manojt2409@gmail.com>
2024-09-22 23:59:07 +0530

    testStack.java updated

--- src/dataStructures/Fundamentals/stackAndQueues/stack/testStack.java
+++ src/dataStructures/Fundamentals/stackAndQueues/stack/testStack.java
@@ -1,38 +1,36 @@
 package dataStructures.Fundamentals.stackAndQueues.stack;
 
-import org.junit.Assert;
-import org.junit.Test;
 
 public class testStack {
-
-    @Test
+    public static void main(String[] args) {
+        testStack obj=new testStack();
+        obj.Constructor();
+        obj.Push();
+        obj.Pop();
+    }
+    
     public void Constructor(){
         Stack stack1=new Stack(4);
         int peekValue=stack1.peek();
+        System.out.println(peekValue);
         int heightValue=stack1.getHeight();
-        Assert.assertEquals(4,peekValue);
-        Assert.assertEquals(1,heightValue);
+        System.out.println(heightValue);
     }
 
-    @Test
+
     public void Push() {
         Stack stack = new Stack(5);
         stack.push(10);
         stack.printAll();
-        Assert.assertEquals(10,stack.peek());
-        Assert.assertEquals(2,stack.getHeight());
-    }
-    @Test
-    public void pop(){
+       }
+
+    public void Pop(){
         Stack stack = new Stack(5);
         stack.push(10);
         stack.printAll();
-        Assert.assertEquals(10,stack.peek());
-        Assert.assertEquals(2,stack.getHeight());
         stack.pop();
-        Assert.assertEquals(5,stack.peek());
-        Assert.assertEquals(1,stack.getHeight());
+        stack.printAll();
         stack.pop();
-        Assert.assertEquals(0,stack.getHeight());
+        stack.printAll();
     }
 }

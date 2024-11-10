b284a3e55575d8ff42e7e7dd12f9675c351f2a95
Manoj Thiyagarajan <manojt2409@gmail.com>
2024-11-10 16:51:58 +0530

    updated HashTable.java

---
+++ src/dataStructures/CollectionsDataStructure/HashTable/test.java
@@ -0,0 +1,15 @@
+package dataStructures.CollectionsDataStructure.HashTable;
+
+public class test {
+    public static void main(String[] args) {
+        HashTable hashTable=new HashTable();
+        hashTable.set("nails",10);
+        hashTable.set("nails",10);
+        hashTable.set("nut",1);
+        hashTable.set("Thor",3);
+        /*hashTable.printTable();
+        System.out.println(hashTable.get("nails"));
+        System.out.println(hashTable.get("something"));*/
+        System.out.println(hashTable.keys());
+    }
+}

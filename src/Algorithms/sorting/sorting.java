ece9e388e70b2eb2dfd662cd81337eefba56dc49
Manoj Thiyagarajan <manojt2409@gmail.com>
2024-11-24 18:17:04 +0530

    updated sort - quick sort

--- src/Algorithms/sorting/sorting.java
+++ src/Algorithms/sorting/sorting.java
@@ -6,18 +6,16 @@ public class sorting {
     public static void main(String[] args) {
         int arr[]={10,8,9,4,6,2,5,8,1,2,0,-5};
         int result[]={-5,0,1,2,2,4,5,6,8,8,9,10};
+        printTheArray(result);
         //printTheArray(bubbleSort(arr));
         //printTheArray(selectionSort(arr));
         //printTheArray(insertionSort(arr));
-        printTheArray(result);
-        System.out.println();
-        printTheArray((mergeSort(arr)));
+        //printTheArray((mergeSort(arr)));
+        printTheArray(quickSort(arr));
 
     }
     public static void printTheArray(int[] arr){
-        for(int number : arr){
-            System.out.print(number+",");
-        }
+        System.out.println(Arrays.toString(arr));
     }
     public static void swap(int arr[],int i,int j){
         int temp=arr[i];
@@ -93,19 +91,41 @@ public class sorting {
         if(array.length==1)return array;
 
         int mid=array.length/2;
-        int left[]=mergeSort(Arrays.copyOfRange(array,0,mid));
+        int left[]=mergeSort(Arrays.copyOfRange(array,0,mid));//first returns left part of sorted array and proceeds further
         int right[]=mergeSort(Arrays.copyOfRange(array,mid,array.length));
 
         return merge(left,right);
     }
+        //quick sort
+        private static int pivot(int[] array, int pivotIndex, int endIndex) {
+        //place that value in the respective index
+            int swapIndex = pivotIndex;
+            for (int i = pivotIndex + 1; i <= endIndex; i++) {
+                if (array[i] < array[pivotIndex]) {
+                    swapIndex++;
+                    swap(array, swapIndex, i);
+                }
+            }
+            swap(array, pivotIndex, swapIndex);
+            return swapIndex;
+        }
 
-
-
-
+    private static void quickSortHelper(int[] array, int left, int right) {
+        if (left < right) {
+            int pivotIndex = pivot(array, left, right);
+            quickSortHelper(array, left, pivotIndex-1);
+            quickSortHelper(array, pivotIndex+1, right);
+        }
+    }
+    public static int[] quickSort(int[] arr){
+        quickSortHelper(arr,0, arr.length-1);
+        return arr;
+    }
 
 //bubble sort       : pure brute force
 //selection sort    : select the minimum value from the list and then sort which reduces the number of swaps and more optimal than bubble sort
 //insertion sort    : optimal when the array is partially sorted
 //merge sort        : one of the optimal approach TC: O(n log n) and using recursive strategy
+//quick sort        : used for most unsorted array and not optimal for partially or fully sorted array
 
 }

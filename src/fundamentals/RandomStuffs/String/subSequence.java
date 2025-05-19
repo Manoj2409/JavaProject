38f8f176549debe12471f1ffda2c5d06e550c24d
Manoj Thiyagarajan <manojt2409@gmail.com>
2025-05-20 01:51:07 +0530

    added ascii subSequence

--- src/fundamentals/RandomStuffs/String/subSequence.java
+++ src/fundamentals/RandomStuffs/String/subSequence.java
@@ -1,5 +1,6 @@
 package fundamentals.RandomStuffs.String;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,9 +9,12 @@ public class subSequence {
     static List<String> list =new LinkedList<>();
 
     public static void main(String[] args) {
-        String string="abc";
-        subseq("",string);
-        list.forEach(System.out::println);
+        String string="ab";
+        //subseq("",string);
+        //list.forEach(System.out::println);
+        /*ArrayList<String> list1=subSeqReturn("",string);
+        list1.forEach(System.out::println);*/
+        subseqAcii("",string);
     }
 
     static void subseq(String p,String up){
@@ -22,5 +26,33 @@ public class subSequence {
         subseq(p+ch,up.substring(1));
         subseq(p,up.substring(1));
     }
+    static ArrayList<String> subSeqReturn(String p,String up){
+        if(up.isEmpty()){
+            ArrayList<String> stringArrayList=new ArrayList<>();
+            stringArrayList.add(p);
+            return stringArrayList;
+        }
+        char ch=up.charAt(0);
+        ArrayList<String> left=subSeqReturn(p+ch,up.substring(1));
+        ArrayList<String> right=subSeqReturn(p,up.substring(1));
+        left.addAll(right);
+        return left;
+    }
 
+    //print the ascii values of the characters
+    static void subseqAcii(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch=up.charAt(0);
+        subseqAcii(p+ch,up.substring(1));
+        subseqAcii(p+(ch+0),up.substring(1));
+        subseqAcii(p,up.substring(1));
+    }
 }
+
+
+
+
+
